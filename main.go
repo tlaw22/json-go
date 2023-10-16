1dec2f998d0c5de735ac537b322597ce9acb25b9
Tim <timothy.j.lawless@outlook.com>
2023-10-16 07:49:58 -0400

    LESSON COMPLETE

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 )
 
@@ -13,27 +14,54 @@ type Person struct {
 }
 
 func main() {
-
 	myJson := `
-	[
-		{
-			"first_name": "Clark",
-			"last_name": "Kent",
-			"hair_color: "black",
-			"has_dog": true
-		},
-		{
-			"first_name": "Brue",
-			"last_name": "Wayne",
-			"hair_color: "black",
-			"has_dog": false
-		}
-	]`
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
 
 	var unmarshalled []Person
+
 	err := json.Unmarshal([]byte(myJson), &unmarshalled)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error unmarshalling json", err)
 	}
+
 	log.Printf("unmarshalled: %v", unmarshalled)
+
+	// write json from a struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Diana"
+	m2.LastName = "Prince"
+	m2.HairColor = "black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "     ")
+	if err != nil {
+		log.Println("error marshalling", err)
+	}
+
+	fmt.Println(string(newJson))
 }
